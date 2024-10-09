cad35adb0bf511da192ccb1c4f703b31170fb32c
amirhossein <amirhossein@localhost>
2024-10-09 13:22:18 +0200

    First Commit

--- Group5_HA_IO_RE/src/capstoneProject/LogMetadata.java
+++ Group5_HA_IO_RE/src/capstoneProject/LogMetadata.java
@@ -1,5 +1,38 @@
 package capstoneProject;
 
-public class LogMetadata {
+import java.time.LocalDate;
 
+public class LogMetadata {
+    private String logFileName;
+    private LocalDate date;
+    private String equipmentName;
+    private String energySource;
+
+    public LogMetadata(String equipmentName, String energySource) {
+        this.date = LocalDate.now();
+        this.equipmentName = equipmentName;
+        this.energySource = energySource;
+        this.logFileName = createLogFileName();
+    }
+
+    private String createLogFileName() {
+        return String.format("%s_%s_%s.log", equipmentName, energySource, date);
+    }
+
+    public String getLogFileName() {
+        return logFileName;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public String getEquipmentName() {
+        return equipmentName;
+    }
+
+    public String getEnergySource() {
+        return energySource;
+    }
 }
+
